385ca992704df28175fa91d0fb210989dc231aa4
Vincent Tremblay <vincent2000.tremblay@gmail.com>
2022-11-20 23:56:05 -0500

    creations de nouveaux tests : commentaires++

--- Tests.py
+++ Tests.py
@@ -74,7 +74,7 @@ class TestTwitterAPI(unittest.TestCase):
     def test_create_headers(self):
         self.assertIsNotNone(TwitterAPI.create_twitter_headers())
 
-    # test_query_twitter_api
+    # test_query_twitter_api verifie la création du Json des tweets recherché et verifie que c'est non-null
     def test_query_twitter_api(self):
 
         headers = TwitterAPI.create_twitter_headers()
@@ -83,7 +83,7 @@ class TestTwitterAPI(unittest.TestCase):
 
         self.assertIsNotNone(json_response)
 
-    # test_empty_query_twitter_api
+    # test_empty_query_twitter_api verifie que l'entrée d'une chaine de caractere vide donne un json non-vide
     def test_empty_query_twitter_api(self):
         headers = TwitterAPI.create_twitter_headers()
         url, params = TwitterAPI.create_twitter_url("")
@@ -92,14 +92,14 @@ class TestTwitterAPI(unittest.TestCase):
 
         self.assertIsNotNone(json_response)
 
-    # test_none_query_twitter_api
+    # test_none_query_twitter_api verifie si cree le json des tweets est non-nul
     def test_none_query_twitter_api(self):
         headers = TwitterAPI.create_twitter_headers()
         url, params = TwitterAPI.create_twitter_url(None)
         json_response = TwitterAPI.query_twitter_api(url, headers, params)
         self.assertIsNotNone(json_response)
 
-    # test_find_tweet_from_json
+    # test_find_tweet_from_json test si il est possible de trouver les différents tweets selon les données recherchées
     def test_find_tweet_from_json(self):
         database = Database();
         data = 'salut'
@@ -109,7 +109,11 @@ class TestTwitterAPI(unittest.TestCase):
         tweets = json_response['data']
         self.assertIsNotNone(tweets)
 
-    # test_find_tweet_from_json_none_data
+    # test_find_tweet_from_json_none_data test si il est possible de trouvé les différents tweets
+    #                                     selon un data Null
+    # ce test ne marche pas, on ne comprend pas vraiment comment vraiment faire ce test proprement
+    # car ce le data provient normalement de l'entrée de l'utilisateur et sa definition se fait à
+    # l'intérieur meme du code.
     def test_find_tweet_from_json_none_data(self):
         data = None
         headers = TwitterAPI.create_twitter_headers()
@@ -118,7 +122,11 @@ class TestTwitterAPI(unittest.TestCase):
         tweets = json_response['data']
         self.assertIsNotNone(tweets)
 
-    # test_find_tweet_from_json_empty_data
+    # test_find_tweet_from_json_empty_data test si il est possible de trouvé les différents tweets
+    #                                      selon un data vide.
+
+    # j'ai le meme probleme que pour le test précedent, je ne suis pas sur de savoir comment réelement
+    # faire ce test pour que la solution implémenté dans le code corrige réellement le test.
     def test_find_tweet_from_json_empty_data(self):
         data = ''
         headers = TwitterAPI.create_twitter_headers()
@@ -127,7 +135,11 @@ class TestTwitterAPI(unittest.TestCase):
         tweets = json_response['data']
         self.assertIsNotNone(tweets)
 
-    # test_find_tweet_from_json_space_data
+    # test_find_tweet_from_json_space_data test si il est possible de trouvé les différents tweets
+    #                                      selon un espace.
+    #
+    # j'ai le meme probleme que pour le test précedent, je ne suis pas sur de savoir comment réelement
+    # faire ce test pour que la solution implémenté dans le code corrige réellement le test.
     def test_find_tweet_from_json_space_data(self):
         data = ' '
         headers = TwitterAPI.create_twitter_headers()
@@ -136,7 +148,8 @@ class TestTwitterAPI(unittest.TestCase):
         tweets = json_response['data']
         self.assertIsNotNone(tweets)
 
-    #test_twitter_url_one_word
+    #test_twitter_url_one_word test pour voir si la fonction de créé le url fonctionne avec un
+    # seul mot en query
     def test_twitter_url_one_word(self):
         self.assertEqual(('https://api.twitter.com/2/tweets/search/recent', {
             'query': "test",
@@ -147,9 +160,10 @@ class TestTwitterAPI(unittest.TestCase):
             'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
             'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
             'next_token': {}
-        }), TwitterAPI.create_twitter_url(["Test", "Test2"]))
+        }), TwitterAPI.create_twitter_url("test"))
 
-    # test_twitter_url_multiple_words
+    # test_twitter_url_multiple_words test pour voir si la fonction de créé le url fonctionne avec
+    # plusieurs mots en query
     def test_twitter_url_multiple_words(self):
         self.assertEqual(('https://api.twitter.com/2/tweets/search/recent', {
             'query': "test for multiples queries",
@@ -162,7 +176,8 @@ class TestTwitterAPI(unittest.TestCase):
             'next_token': {}
         }), TwitterAPI.create_twitter_url("test for multiples queries"))
 
-    # test_twitter_base_param
+    # test_twitter_base_param test pour voir si la fonction créé le url fonctionne avec
+    # une quantité de result max de base.
     def test_twitter_base_param(self):
         self.assertEqual(('https://api.twitter.com/2/tweets/search/recent', {
             'query': "test",
@@ -175,7 +190,8 @@ class TestTwitterAPI(unittest.TestCase):
             'next_token': {}
         }), TwitterAPI.create_twitter_url("test"))  # laisser aux valeur de max_results par défault
 
-    # test_twitter_custom_param
+    # test_twitter_custom_param test pour voir si la fonction créé le url fonctionne avec
+    # une quantité de result max determiné.
     def test_twitter_custom_param(self):
         self.assertEqual(('https://api.twitter.com/2/tweets/search/recent', {
             'query': "test",
@@ -188,7 +204,8 @@ class TestTwitterAPI(unittest.TestCase):
             'next_token': {}
         }), TwitterAPI.create_twitter_url("test", 15))  # max_results modifier à 15
 
-    # test_twitter_weird_character_query
+    # test_twitter_weird_character_query test pour voir si la fonction de créé le url fonctionne avec
+    # un caractere spécial dans le query.
     def test_twitter_weird_character_query(self):
         self.assertEqual(('https://api.twitter.com/2/tweets/search/recent', {
             'query': ",",
@@ -201,7 +218,8 @@ class TestTwitterAPI(unittest.TestCase):
             'next_token': {}
         }), TwitterAPI.create_twitter_url(","))
 
-    # test_twitter_none_character_query
+    # test_twitter_none_character_query test pour voir si la fonction de créé le url fonctionne avec
+    # une valeur null dans le query.
     def test_twitter_none_character_query(self):
         self.assertEqual(('https://api.twitter.com/2/tweets/search/recent', {
             'query': " ",
